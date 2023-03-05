9fa04848153a247fd423b2691eeaecdec39ec5fc
Fabian Beltran <fabianbeltran1224@gmail.com>
2023-03-05 12:27:03 -0500

    adding bar percentage

--- Frontend/src/components/QuizHeader.vue
+++ Frontend/src/components/QuizHeader.vue
@@ -1,14 +1,15 @@
 <script setup>
-import { defineProps } from "vue";
-
-const { questionStatus } = defineProps(["questionStatus"]);
+const { questionStatus, barPercentage } = defineProps([
+  "questionStatus",
+  "barPercentage",
+]);
 </script>
 
 <template>
   <header>
     <h4>{{ questionStatus }}</h4>
     <div class="bar">
-      <div class="completion"></div>
+      <div class="completion" :style="{ width: barPercentage }"></div>
     </div>
   </header>
 </template>
--- Frontend/src/views/QuizView.vue
+++ Frontend/src/views/QuizView.vue
@@ -2,22 +2,32 @@
 import Question from "../components/Question.vue";
 import QuizHeader from "../components/QuizHeader.vue";
 import { useRoute } from "vue-router";
-import { ref } from "vue";
+import { ref, watch, computed } from "vue";
 import quizes from "../data/quizes.json";
 
 const route = useRoute();
 const quizId = parseInt(route.params.id);
 const quiz = quizes.find((q) => q.id === quizId);
 const currentQuestionIndex = ref(0);
-const questionStatus = `${currentQuestionIndex.value}/${quiz.questions.length}`;
+const questionStatus = computed(
+  () => `${currentQuestionIndex.value}/${quiz.questions.length}`
+);
+
+const barPercentage = computed(
+  () => `${(currentQuestionIndex.value / quiz.questions.length) * 100}%`
+);
 </script>
 
 <template>
   <div>
-    <QuizHeader :questionStatus="questionStatus" />
+    <QuizHeader
+      :questionStatus="questionStatus"
+      :barPercentage="barPercentage"
+    />
     <div>
       <Question :question="quiz.questions[currentQuestionIndex]" />
     </div>
+    <button @click="currentQuestionIndex++">Next Question</button>
   </div>
 </template>
 
